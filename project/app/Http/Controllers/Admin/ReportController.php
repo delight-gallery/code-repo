7560b1711b3f0520f08e2da597243acaac805ba5
Deffrin <deffrin@gmail.com>
2020-02-28 08:40:41 +0530

    payment report excel

--- project/app/Http/Controllers/Admin/ReportController.php
+++ project/app/Http/Controllers/Admin/ReportController.php
@@ -9,6 +9,8 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Input;
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
 
 class ReportController extends Controller
 {
@@ -94,32 +96,106 @@ class ReportController extends Controller
 
     public function download()
     {
-        // $order = new Order;
+        $order = new Order;
         
-        // $order->with('vendororders');
-        // $order->with('user');
+        $order->with('vendororders');
+        $order->with('user');
         
-        // $order->where('pay_amount','>',0);
+        $order->where('pay_amount','>',0);
 
-        // if (!empty($request->form)) {
-        //     parse_str($request->form, $form);
-        // }
-        // // print_r($form);
-        // if (empty($form['fromdate']) && empty($form['todate'])) {
-        //     $order = $order->whereRaw('MONTH(created_at) = ?',[date('m')]);
-        // } else {
-        //     if (!empty($form['fromdate']) && empty($form['todate'])) {
-        //         $order = $order->whereRaw('DATE(created_at) >= ?', [date('Y-m-d',strtotime($form['fromdate']))]);
-        //     } else if (!empty($form['fromdate']) && !empty($form['todate'])) {
-        //         $order = $order->whereDate('created_at', '>=', date('Y-m-d',strtotime($form['fromdate'])));
-        //         $order = $order->whereDate('created_at', '<=', date('Y-m-d',strtotime($form['todate'])));
-        //     }
-        // }
+        if (!empty($request->form)) {
+            parse_str($request->form, $form);
+        }
         
+        $datas = $order->orderBy('id','desc')->get();
+        //--- Integrating This Collection Into Datatables
+        
+        $spreadsheet = new Spreadsheet();
+        $sheet = $spreadsheet->getActiveSheet();
 
-        // $datas = $order->orderBy('id','desc')->get();
-        //  //--- Integrating This Collection Into Datatables
-         
+        $sheet->getColumnDimension('A')->setWidth(20);
+        $sheet->getColumnDimension('B')->setWidth(20);
+        $sheet->getColumnDimension('C')->setWidth(20);
+        $sheet->getColumnDimension('D')->setWidth(30);
+        $sheet->getColumnDimension('E')->setWidth(20);
+        $sheet->getColumnDimension('F')->setWidth(30);
+        $sheet->getColumnDimension('G')->setWidth(20);
+        $sheet->getColumnDimension('H')->setWidth(20);
+
+        $column_headings = [
+            'Vendor',
+            'Order No.',
+            'Invoice No.',
+            'Customer',
+            'Payment Amount',
+            'Payment Method',
+            'Order Date',
+            'Payment Status',
+        ];
+
+        $column = "A";
+        $line = 1;
+        foreach ($column_headings as $key => $column_heading) {
+            $sheet->setCellValue($column.$line, $column_heading);
+            $column++;
+        }
+        
+        $line++;
+        
+        foreach ($datas as $key => $data) {
+            
+            $column = "A";
+            $vendor_name = '';
+            if (isset($data->vendororders[0])) {
+                $vendor = User::find($data->vendororders[0]->user_id)->first();
+                if (isset($vendor->name)) {
+                    $vendor_name = $vendor->name;
+                } else {
+                    $vendor_name = '';
+                }
+            }
+            $sheet->setCellValue($column.$line, $vendor_name);
+            $column++;
+
+            $order_number = $data->order_number;
+            $sheet->setCellValue($column.$line, $order_number);
+            $column++;
+
+            $invoice_number = sprintf("%'.08d", $data->id);
+            $sheet->setCellValue($column.$line, $invoice_number);
+            $column++;
+
+            if (isset($data->user->name)) {
+                $customer = $data->user->name;
+            } else {
+                $customer = '';
+            }
+            $sheet->setCellValue($column.$line, $customer);
+            $column++;
+            
+            $sheet->setCellValue($column.$line, $data->pay_amount);
+            $column++;
+
+            $sheet->setCellValue($column.$line, $data->method);
+            $column++;
+
+            $sheet->setCellValue($column.$line, date('d-m-Y', strtotime($data->created_at)));
+            $column++;
+
+            $sheet->setCellValue($column.$line, ucfirst($data->payment_status));
+            $column++;
+
+            $line++;
+        }
+        
+        $writer = new Xlsx($spreadsheet);
+        
+        $filename = storage_path('app/Payment Report.xlsx');
+        
+        $writer->save($filename);
+
+        return response()->download($filename);
+
     }
 
 }
